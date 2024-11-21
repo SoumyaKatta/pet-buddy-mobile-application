90db050751b51f76b05a2562be5d127a8b9d514d
SoumyaKatta <soumya.katta@everest.engineering>
2024-11-21 17:56:09 +0530

    Create Text PetBuddy using Text Component

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,6 +1,13 @@
-export function Login(){
-    return(
-        <>
-        </>
-    )
+import {Text, View} from 'react-native';
+
+export function Login() {
+  return (
+    <>
+      <View style={{top: 140, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{fontSize: 30, fontWeight: 'bold', color: '#32cd32'}}>
+          PetBuddy!
+        </Text>
+      </View>
+    </>
+  );
 }
