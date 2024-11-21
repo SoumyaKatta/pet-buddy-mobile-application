7a99917eb414122c4abf114c9394cfe835f8387b
SoumyaKatta <soumya.katta@everest.engineering>
2024-11-21 18:15:48 +0530

    Create the Login button element

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,4 +1,4 @@
-import {Text, TextInput, View} from 'react-native';
+import {Text, TextInput, TouchableOpacity, View} from 'react-native';
 
 export function Login() {
   return (
@@ -8,13 +8,44 @@ export function Login() {
           PetBuddy!
         </Text>
         <TextInput
-          style={{height: 45, width: 300, borderWidth: 1, marginTop: 30,padding:10,fontSize:20,fontWeight:'bold',borderRadius:10}}
+          style={{
+            height: 45,
+            width: 300,
+            borderWidth: 1,
+            marginTop: 30,
+            padding: 10,
+            fontSize: 20,
+            fontWeight: 'bold',
+            borderRadius: 10,
+          }}
           placeholder="Username"
         />
         <TextInput
-          style={{height: 45, width: 300, borderWidth: 1, marginTop: 30,padding:10,fontSize:20,fontWeight:'bold',borderRadius:10}}
+          style={{
+            height: 45,
+            width: 300,
+            borderWidth: 1,
+            marginTop: 30,
+            padding: 10,
+            fontSize: 20,
+            fontWeight: 'bold',
+            borderRadius: 10,
+          }}
           placeholder="Password"
         />
+        <TouchableOpacity
+          style={{
+            height: 60,
+            width: 250,
+            marginTop: 50,
+            padding: 10,
+            borderRadius: 10,
+            justifyContent:'center',
+            alignItems:'center',
+            backgroundColor:'#32cd32',
+          }}>
+          <Text style={{color:'white', fontSize:30, fontWeight:"bold"}}>Login</Text>
+        </TouchableOpacity>
       </View>
     </>
   );
