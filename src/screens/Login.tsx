f4347995c7ab985829425adc7765cbac8062f0d3
SoumyaKatta <soumya.katta@everest.engineering>
2024-11-21 18:29:37 +0530

    Create a stylesheet for putting all styles in that

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,62 +1,61 @@
-import {Button, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 export function Login() {
   return (
     <>
-      <View style={{top: 140, justifyContent: 'center', alignItems: 'center'}}>
-        <Text style={{fontSize: 30, fontWeight: 'bold', color: '#32cd32'}}>
-          PetBuddy!
-        </Text>
-        <TextInput
-          style={{
-            height: 45,
-            width: 300,
-            borderWidth: 1,
-            marginTop: 30,
-            padding: 10,
-            fontSize: 20,
-            fontWeight: 'bold',
-            borderRadius: 10,
-          }}
-          placeholder="Username"
-        />
-        <TextInput
-          style={{
-            height: 45,
-            width: 300,
-            borderWidth: 1,
-            marginTop: 30,
-            padding: 10,
-            fontSize: 20,
-            fontWeight: 'bold',
-            borderRadius: 10,
-          }}
-          placeholder="Password"
-        />
-        <TouchableOpacity
-          style={{
-            height: 60,
-            width: 250,
-            marginTop: 50,
-            padding: 10,
-            borderRadius: 10,
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: '#32cd32',
-          }}>
-          <Text style={{color: 'white', fontSize: 30, fontWeight: 'bold'}}>
-            Login
-          </Text>
+      <View style={styles.container}>
+        <Text style={styles.petBuddyText}>PetBuddy!</Text>
+        <TextInput style={styles.usernameText} placeholder="Username" />
+        <TextInput style={styles.usernameText} placeholder="Password" />
+        <TouchableOpacity style={styles.loginButton}>
+          <Text style={styles.loginText}>Login</Text>
         </TouchableOpacity>
-        <View style={{display: 'flex', flexDirection: 'row', marginTop: 20}}>
-          <Text style={{fontSize: 19, color: '#32cd32'}}>
-            Don't have an account?
-          </Text>
+        <View style={styles.innerContainer}>
+          <Text style={styles.account}>Don't have an account?</Text>
           <TouchableOpacity>
-            <Text style={{fontSize: 19, color: '#32cd32'}}> Register</Text>
+            <Text style={styles.registerText}> Register</Text>
           </TouchableOpacity>
         </View>
       </View>
     </>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    top: 140,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  petBuddyText: {fontSize: 30, fontWeight: 'bold', color: '#32cd32'},
+  usernameText: {
+    height: 45,
+    width: 300,
+    borderWidth: 1,
+    marginTop: 30,
+    padding: 10,
+    fontSize: 20,
+    fontWeight: 'bold',
+    borderRadius: 10,
+  },
+  loginButton: {
+    height: 60,
+    width: 250,
+    marginTop: 50,
+    padding: 10,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#32cd32',
+  },
+  loginText: {color: 'white', fontSize: 30, fontWeight: 'bold'},
+  innerContainer: {display: 'flex', flexDirection: 'row', marginTop: 20},
+  account: {fontSize: 19, color: '#32cd32'},
+  registerText: {fontSize: 19, color: '#32cd32'},
+});
