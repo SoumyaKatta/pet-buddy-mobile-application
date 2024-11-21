e84927493698a02aa719faa0413d5f02d1fdc0a5
SoumyaKatta <soumya.katta@everest.engineering>
2024-11-21 16:42:06 +0530

    Create a stylesheet to keep all the styles

--- src/screens/Welcom.tsx
+++ src/screens/Welcom.tsx
@@ -1,22 +1,32 @@
-import {Dimensions, ImageBackground, Text, View} from 'react-native';
+import {
+  Dimensions,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
+const screenHeight = Dimensions.get('window').height;
+const screenWidth = Dimensions.get('window').width;
 export function Welcome() {
-  const screenHeight = Dimensions.get('window').height;
-  const screenWidth = Dimensions.get('window').width;
   return (
     <>
       <View>
         <ImageBackground
-          style={{
-            height: screenHeight,
-            width: screenWidth,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-          testID='background-image'
+          style={styles.backgroundImage}
+          testID="background-image"
           source={require('../../assets/images/pet.jpg')}
           resizeMode="stretch"></ImageBackground>
       </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    height: screenHeight,
+    width: screenWidth,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
