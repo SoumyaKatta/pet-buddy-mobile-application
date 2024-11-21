2a62fb089989b3aec0fd9ceee990d4ef7b18d598
SoumyaKatta <soumya.katta@everest.engineering>
2024-11-21 16:35:53 +0530

    Add the background petImage

--- src/screens/Welcom.tsx
+++ src/screens/Welcom.tsx
@@ -1,7 +1,22 @@
-export function Welcome(){
-    return(
-        <>
-        
-        </>
-    )
+import {Dimensions, ImageBackground, Text, View} from 'react-native';
+
+export function Welcome() {
+  const screenHeight = Dimensions.get('window').height;
+  const screenWidth = Dimensions.get('window').width;
+  return (
+    <>
+      <View>
+        <ImageBackground
+          style={{
+            height: screenHeight,
+            width: screenWidth,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          testID='background-image'
+          source={require('../../assets/images/pet.jpg')}
+          resizeMode="stretch"></ImageBackground>
+      </View>
+    </>
+  );
 }
